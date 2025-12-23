fb3c96e2dc8077e722618e78bcecf00e614f2df4
Rabbi <fazle.rabbi@bracits.com>
2025-12-23 16:23:33 +0600

    fix controller issues

--- src/main/java/org/ostad/ostad_assigment_18/controller/StatusController.java
+++ src/main/java/org/ostad/ostad_assigment_18/controller/StatusController.java
@@ -1,4 +1,25 @@
 package org.ostad.ostad_assigment_18.controller;
 
+import org.ostad.ostad_assigment_18.service.WelcomeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class StatusController {
+    private final WelcomeService welcomeService;
+
+    public StatusController(WelcomeService welcomeService) {
+        this.welcomeService = welcomeService;
+    }
+
+
+    @GetMapping("/api/status")
+    public Map<String, String> status() {
+        return Map.of(
+                "message", welcomeService.getMessage(),
+                "apiUrl", welcomeService.getApiUrl()
+        );
+    }
 }
