4f2b7a89273871873a878721e6d728243d803b0c
Rabbi <fazle.rabbi@bracits.com>
2025-12-23 13:51:37 +0600

    first commit

---
+++ src/main/java/org/ostad/ostad_assigment_18/OstadAssigment18Application.java
@@ -0,0 +1,13 @@
+package org.ostad.ostad_assigment_18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OstadAssigment18Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OstadAssigment18Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/ostad/ostad_assigment_18/OstadAssigment18ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ostad.ostad_assigment_18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OstadAssigment18ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
